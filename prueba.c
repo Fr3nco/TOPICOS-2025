d7cab6774f538fef0317cfc99924fb89d5b85e8a
Fr3nco <167140559+Fr3nco@users.noreply.github.com>
2025-06-06 14:43:30 -0300

    actualización funcion1
    mande el registro a un string y lo recorro con punteros para que sea mas legible y menos raro.

---
+++ prueba.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM_REGISTRO 501
+#define ERROR_ARCHIVO 1
+
+typedef struct {
+    int d, m, a;
+} Fecha;
+
+typedef struct {
+    Fecha Fingr;
+    char nombre[31];
+    int indice;
+} VarCosto;
+
+int FuncionGenerica(size_t tamreg);
+
+int main()
+{
+
+FuncionGenerica(sizeof(VarCosto));
+
+    return 0;
+}
+
+int FuncionGenerica(size_t tamreg)
+    {
+    void* reg = malloc(tamreg);
+    if (!reg) return 2;
+
+    FILE* fpIn = fopen("indices_icc_general_capitulos.csv", "r");
+    if (!fpIn)
+    {
+        free(reg);
+        printf("No se pudo abrir el archivo de entrada.\n");
+        return ERROR_ARCHIVO;
+    }
+
+    FILE* fpOut = fopen("archivoNuevo.csv", "w");
+    if (!fpOut)
+    {
+        fclose(fpIn);
+        free(reg);
+        printf("No se pudo crear el archivo de salida.\n");
+        return ERROR_ARCHIVO;
+    }
+
+    VarCosto* var = reg;
+    char registro[TAM_REGISTRO];
+
+    // Copia la cabecera en el archivoNuevo
+    if (fgets(registro, TAM_REGISTRO, fpIn))
+    {
+        fprintf(fpOut, "%s", registro);
+    }
+
+    while (fgets(registro, TAM_REGISTRO, fpIn))
+    {
+
+         char* act = strchr(registro, '\n');
+       *act = '\0';
+
+
+        act = strrchr(registro, ';');
+        *act = '\0';
+        sscanf(act + 1, "%d", &var->indice);
+
+
+        act = strrchr(registro, ';');
+        *act = '\0';
+        strcpy(var->nombre, act + 1);
+
+
+        sscanf(registro, "\"%d/%d/%d\"", &var->Fingr.d, &var->Fingr.m, &var->Fingr.a);
+
+
+        fprintf(fpOut, "\"%04d/%02d/%02d\";%s;%d\n",var->Fingr.a, var->Fingr.m, var->Fingr.d,var->nombre, var->indice);
+
+
+
+    }
+
+    fclose(fpIn);
+    fclose(fpOut);
+    free(reg);
+
+    return 0;
+}
