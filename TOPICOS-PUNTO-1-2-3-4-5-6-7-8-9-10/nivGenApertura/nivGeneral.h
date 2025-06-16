a665ded0f65c648c1376690082f720375786b8df
Fr3nco <167140559+Fr3nco@users.noreply.github.com>
2025-06-16 20:29:44 -0300

    Actulizacion y orden

---
+++ TOPICOS-PUNTO-1-2-3-4-5-6-7-8-9-10/nivGenApertura/nivGeneral.h
@@ -0,0 +1,30 @@
+#ifndef NIVGENERAL_H_INCLUDED
+#define NIVGENERAL_H_INCLUDED
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    char* cursor;
+    bool secFin;
+    int pos;
+
+}secuenciaPalabras;
+
+typedef struct
+{
+    char vPal[51];
+}Palabra;
+
+void secuenciaPalabrasCrear(secuenciaPalabras* sec, char* cad);
+bool secuenciaPalabrasDesencriptar(secuenciaPalabras* sec, Palabra* pal);
+bool secuenciaPalabrasEscribir(secuenciaPalabras* sec, Palabra* pal);
+void secuenciaPalabrasEscribirCaracter(secuenciaPalabras* sec, char c);
+bool secuenciaPalabrasFin(secuenciaPalabras* sec);
+void secuenciaPalabrasCerrar(secuenciaPalabras* sec);
+
+void palabraATitulo(Palabra* pal);
+bool esLetra(char c);
+
+#endif // NIVGENERAL_H_INCLUDED
